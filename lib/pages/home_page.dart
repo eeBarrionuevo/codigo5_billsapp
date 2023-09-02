015bf4ddf7ff9a9267412048aebaff2699d03d83
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-09-01 20:26:28 -0500

    Mods

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -31,16 +31,65 @@ class HomePage extends StatelessWidget {
               Expanded(
                 child: Container(
                   width: double.infinity,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(34),
                       bottomRight: Radius.circular(34),
                     ),
                   ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [],
+                  child: SafeArea(
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Resumen de gastos",
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Gestiona tus gastos de la mejor forma",
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black45,
+                              ),
+                            ),
+                            ListTile(
+                              leading: CircleAvatar(),
+                              title: Text(
+                                "Compras en el super",
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color(0xff101321),
+                                ),
+                              ),
+                              subtitle: Text(
+                                "14/01/2023 23:21 PM",
+                                style: TextStyle(
+                                  fontSize: 13.0,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xff101321).withOpacity(0.70),
+                                ),
+                              ),
+                              trailing: Text(
+                                "S/ 400.00",
+                                style: TextStyle(
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color(0xff101321),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
