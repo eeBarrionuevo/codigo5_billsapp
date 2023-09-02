82f827be5b49845b4986f8785f0e9a4f9a0ac6d6
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-09-01 20:45:35 -0500

    Mods

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -62,6 +62,33 @@ class HomePage extends StatelessWidget {
                                 color: Colors.black45,
                               ),
                             ),
+                            const SizedBox(
+                              height: 16.0,
+                            ),
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: "Buscar por título",
+                                hintStyle: TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.black.withOpacity(0.40),
+                                ),
+                                filled: true,
+                                fillColor:
+                                    const Color(0xff101321).withOpacity(0.05),
+                                contentPadding: const EdgeInsets.all(12.0),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                  borderSide: BorderSide.none,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 16.0,
+                            ),
                             ItemBillWidget(),
                             ItemBillWidget(),
                             ItemBillWidget(),
